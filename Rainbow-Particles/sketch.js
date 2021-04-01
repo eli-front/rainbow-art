d8821dd0ff00b5966bafebba38d6c5c610a5423a
eli-front <elifront23@gmail.com>
2021-03-31 22:27:30 -0500

    particle sliders

--- Rainbow-Particles/sketch.js
+++ Rainbow-Particles/sketch.js
@@ -1,22 +1,46 @@
 let balls = [];
-const connectionVal = 2;
-const ballCount = 100;
-const alphaValue =  30;
+const connectionVal = 3;
+// const ballCount = 50;
+// const alphaValue =  30;
+
+let countSlider;
+let alphaSlider;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight-60);
+  countSlider = createSlider(0, 500, 100, 1);
+  countSlider.position(15, windowHeight - 50);
+  countSlider.style('width', `${width-30}px`);
+  alphaSlider = createSlider(0, 255, 30, 1);
+  alphaSlider.position(15, windowHeight - 30);
+  alphaSlider.style('width', `${width-30}px`);
   
-  
-  for (let i = 0; i < ballCount; i++) balls.push(new Ball(random(width-20)+10, random(height-20)+10));
+  for (let i = 0; i < countSlider.value(); i++) balls.push(new Ball(random(width-20)+10, random(height-20)+10));
 }
 
 function draw() {
-  background(220, 220, 220, alphaValue);
+  if (width != windowWidth || height != windowHeight-60) {
+    resizeCanvas(windowWidth, windowHeight-60);
+    
+    countSlider.position(15, height + 10);
+    countSlider.style('width', `${width-30}px`);
+    
+    alphaSlider.position(15, height + 30);
+    alphaSlider.style('width', `${width-30}px`);
+  }
+  background(220, 220, 220, alphaSlider.value());
   
   balls.forEach((e) => {
     e.update();
     e.show();
   });
+  while (countSlider.value() > balls.length) {
+    balls.push(new Ball(random(width-20)+10, random(height-20)+10));
+  }
+  
+  while (countSlider.value() < balls.length) {
+    balls.shift();
+  }
 }
 
 function mouseClicked() {
